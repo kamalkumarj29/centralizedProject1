118171d5f05adf399e215c04a1ee7f7dc6dd835c
kamal <kamalkumarj29@gmail.com>
2023-02-17 12:41:51 +0530

    newec2

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,9 @@ resource "aws_instance" "web" {
   ami           = "ami-0e742cca61fb65051" 
   instance_type = "t2.micro"
   }
+
+resource "aws_instance" "dev" {
+  ami           = "ami-0e742cca61fb65051"
+  instance_type = "t3.micro"
+  }
+
